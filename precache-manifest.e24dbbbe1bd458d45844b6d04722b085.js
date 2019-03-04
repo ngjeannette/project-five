67741a3e0cca31920c8c1b46510700488f3e8454
Jeannette <jeannetteshui.ng@gmail.com>
2019-03-03 20:53:45 -0500

    Updates

---
+++ precache-manifest.e24dbbbe1bd458d45844b6d04722b085.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "995d9b60b15b57f5b4be",
+    "url": "/jeannette_ng_project5/static/js/runtime~main.995d9b60.js"
+  },
+  {
+    "revision": "53c1d4f2ee9b93ebd407",
+    "url": "/jeannette_ng_project5/static/js/main.53c1d4f2.chunk.js"
+  },
+  {
+    "revision": "877b61a093f436dc65fb",
+    "url": "/jeannette_ng_project5/static/js/2.877b61a0.chunk.js"
+  },
+  {
+    "revision": "53c1d4f2ee9b93ebd407",
+    "url": "/jeannette_ng_project5/static/css/main.ee1dfeb0.chunk.css"
+  },
+  {
+    "revision": "14f95561bbfec277f79ecf7b75cbe6f7",
+    "url": "/jeannette_ng_project5/index.html"
+  }
+];
